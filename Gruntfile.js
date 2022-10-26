e96daca0a244d5b51b0f58e7cf0313eecfe9abd5
Carly <104537084+carly-kins@users.noreply.github.com>
2022-10-26 11:35:00 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+const sass = require('node-sass');
+
+module.exports = function(grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    // Project configuration.
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      clean: {
+        folder: ['pattern_exports/', 'public/'],
+        css: ['source/css/style.css', 'source/css/style.css.map'],
+        meta: ['source/_meta/*.mustache'],
+        js: ['source/js/main.js']
+      },
+      // Turns SCSS to CSS 
+      sass: {
+        dist: {
+          options: {
+            implementation: sass,
+            outputStyle: 'compact',
+            sourceComments: false,
+            sourceMap: true
+          },
+          files: {
+            'source/css/style.css': 'source/css/style.scss'
+          }
+        }
+      },
+      //Build process
+      // Minifies CSS
+      cssmin: {
+        sitecss: {
+          options: {
+            banner: ''
+          },
+          files: {
+            'pattern_exports/css/style.css': [
+              'public/css/style.css'
+            ]
+          }
+        }
+      },
+      // Combines and condenses JS
+      uglify: {
+        options: {
+          compress: true
+        },
+        applib: {
+          src: [
+            'node_modules/jquery/dist/jquery.slim.min.js',
+            'node_modules/@popperjs/core/dist/umd/popper.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/bootstrap/js/dist/dropdown.js',
+            'source/js/custom_js/*.js'
+          ],
+          dest: 'source/js/main.js'
+        }
+      },
+      imagemin: {
+        dynamic: {
+          files: [{
+            expand: true,
+            cwd: 'public/images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'pattern_exports/images/'
+          }]
+        }
+      },
+      // Copy Grunt build processesses.
+      copy: {
+        main: {
+          expand: true,
+          cwd: 'public',
+          src: ['*.ico', '*.svg'],
+          dest: 'pattern_exports/images/'
+        },
+        js: {
+          expand: true,
+          cwd: 'public/js/',
+          src: ['*.js'],
+          dest: 'pattern_exports/js/'
+        },
+      // Make .mustache match .hbs
+        mustache: {
+          expand: true,
+          flatten: true,
+          cwd: 'source',
+          src: ['**/head.hbs', '**/foot.hbs'],
+          dest: 'source/_meta/',
+          rename: function(dest, matchedSrcPath) {
+            if (matchedSrcPath.substring(0, 1) !== '_') {
+              return dest + '_' + matchedSrcPath.replace('.hbs', '.mustache');
+            }
+          }
+        },
+    }
+  });
+  // Default task(s).
+  grunt.registerTask('default', ['clean']);
+  grunt.registerTask('source', ['sass', 'copy:mustache', 'uglify']);
+  grunt.registerTask('public', ['cssmin', 'imagemin', 'copy:main', 'copy:js'])
+};
---
+++ README.md
@@ -0,0 +1,50 @@
+<h1 align="center">Pattern-Lab Setup 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  </a>
+  <a href="https://twitter.com/Carly_kins_" target="_blank">
+    <img alt="Twitter: carly_kins_" src="https://img.shields.io/twitter/follow/carly_kins_.svg?style=social" />
+  </a>
+</p>
+
+> A experimental setup of Patternlab using grunt.js to automate final build.
+
+## Install
+
+```sh
+npm install
+```
+
+## Develop
+
+```sh
+npm run dev
+```
+
+## Build
+
+```sh
+npm run build
+```
+
+## Clean
+
+```sh
+grunt clean
+```
+
+## Author
+
+👤 **Carly Wooten**
+
+* Website: www.carlyannewooten.com
+* Twitter: [@carly\_kins\_](https://twitter.com/carly\_kins\_)
+* Github: [@carly-kins](https://github.com/carly-kins)
+* LinkedIn: [@carly-anne-wooten](https://linkedin.com/in/carly-anne-wooten)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
